3a8bb21fbfa44742fc0e814415c2f6011a7db170
Nanaya <seit@ualberta.ca>
2023-01-09 17:39:48 -0700

    Second Commit

--- lab1.py
+++ lab1.py
@@ -1,3 +1,3 @@
 import requests
-print(requests.__version__)
-print(requests.get("https://google.com/"))
+
+print(requests.get("https://raw.githubusercontent.com/ivanseit2001/404-LAB-01/master/lab1.py").content)
